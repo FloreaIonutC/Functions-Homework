9d63abbe9f9f4eb2836d623bdad0c798014afe40
FloreaIonutC <58517018+FloreaIonutC@users.noreply.github.com>
2020-03-22 22:59:09 +0200

    Delete Student.java

--- Student.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-public class Student extends Person {
-    public Student(String name){super(name);}
-
-    @Override
-    public String toString() { return "Student" + super.toString(); }
-}
