59a427cf4b216b1bad131f0d02ed532ed88f1e96
FloreaIonutC <58517018+FloreaIonutC@users.noreply.github.com>
2020-03-22 23:00:18 +0200

    Delete Country.java

--- Country.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-public class Country extends Person{
-    public Country(String name){super(name);}
-
-    @Override
-    public String toString() { return "Country" + super.toString(); }
-}
