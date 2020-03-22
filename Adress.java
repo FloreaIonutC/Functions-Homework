84c9f57d90d225a9ef8a6d61c5d53a88fe2c8940
FloreaIonutC <58517018+FloreaIonutC@users.noreply.github.com>
2020-03-22 22:58:50 +0200

    Delete Adress.java

--- Adress.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-public class Adress extends  Person {
-    public Adress(String name){super(name);}
-
-    @Override
-    public String toString() { return "Adress" + super.toString(); }
-}
