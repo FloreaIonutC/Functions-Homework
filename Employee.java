db94c87a2e15f3d62d9969328bd174123bbbcd0a
FloreaIonutC <58517018+FloreaIonutC@users.noreply.github.com>
2020-03-22 22:59:16 +0200

    Delete Employee.java

--- Employee.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-public class Employee extends Person {
-    public Employee(String name) {super(name);}
-
-    @Override
-    public String toString() { return "Employee" + super.toString(); }
-}
